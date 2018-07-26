46d8d035d9439c418ad1de2ceb6195c1988e5e28
Jakub Grzelak <jakubgrzelak04101992@gmail.com>
2018-07-26 09:46:05 +0200

    spellin in firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
-import 'fierbase/auth';
+import 'firebase/auth';
 
 const config = {
   apiKey: 'AIzaSyBRvs17ciAclwSIlkOQkCgM87TyzCdamNw',
